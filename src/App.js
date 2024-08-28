ed03b19571603f1e841bc66cdc72311a3615265b
bvp1518 <bvp18061992@gmail.com>
2024-08-28 17:08:43 +0530

    till-330. Layouts & Nested Routes

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 
 import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
+import RootLayout from './pages/Root';
 
 // const routeDefinations = createRoutesFromElements(
 //   <Route>
@@ -19,8 +20,14 @@ import ProductsPage from './pages/Products';
 // const router = createBrowserRouter(routeDefinations);
 
 const router = createBrowserRouter([
-  { path: '/', element: <HomePage /> },
-  { path: '/products', element: <ProductsPage /> },
+  {
+    path:'/',
+    element:<RootLayout />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/products', element: <ProductsPage /> },
+    ],
+  },  
 ]);
 
 function App() {
