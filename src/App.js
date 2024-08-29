ec948ccfa0799a9054a14e200d59a4aa2b07a3be
bvp1518 <bvp18061992@gmail.com>
2024-08-29 10:14:41 +0530

    till-334. Defining & Using Dynamic Routes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import HomePage from './pages/Home';
 import ProductsPage from './pages/Products';
 import RootLayout from './pages/Root';
 import ErrorPage from './pages/Error';
+import ProductDetailPage from './pages/ProductDetail';
 
 // const routeDefinations = createRoutesFromElements(
 //   <Route>
@@ -28,6 +29,8 @@ const router = createBrowserRouter([
     children: [
       { path: '/', element: <HomePage /> },
       { path: '/products', element: <ProductsPage /> },
+      { path: '/products/:productId', element: <ProductDetailPage />},
+      
     ],
   },  
 ]);
