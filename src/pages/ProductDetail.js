26e9b6344461b4cca1d3ac22d21bc04fa2aae033
bvp1518 <bvp18061992@gmail.com>
2024-08-29 11:40:37 +0530

    till-336. Understanding Relative & Absolute Paths

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+
+import { Link, useParams } from 'react-router-dom';
 
 
- function ProductDetailPage() {
-   const params = useParams();
+function ProductDetailPage() {
+  const params = useParams();
 
- 
+
   return (
-  <>
-  <h1> Product Details!</h1>;
-  <p>{params.productId}</p>
-  </>
- ) }
+    <>
+      <h1> Product Details!</h1>;
+      <p>{params.productId}</p>
+      <p><Link to=".." relative='path'>Back</Link></p>
+    </>
+  )
+}
 
 
 export default ProductDetailPage;
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,17 +1,25 @@
+
+import { Link } from "react-router-dom";
+
+const PRODUCTS = [
+    { id:'p1', title:'Product1' },
+    { id:'p2', title:'Product2' },
+    { id:'p3', title:'Product3' }
+]
+
 function ProductsPage() {
-    return(
-        
+    return (
         <>
-        <h1>The Products Page</h1>
-   <ul>
-    <li>Product 1</li>
-    <li>Product 2</li>
-    <li>Product 3</li>
-
-   </ul>
-
-</>
-    )
-} 
+            <h1>The Products Page</h1>
+            <ul>
+                {PRODUCTS.map((prod) => (
+                    <li key={prod.id}>
+                        <Link to={prod.id}>{prod.title}</Link>
+                    </li>
+                ))}
+            </ul>
+        </>
+    );
+}
 
 export default ProductsPage;
